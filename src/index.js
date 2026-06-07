ca96d8d1ccba3bb7cb08ac8856cf7633fa3a5fdf
Bonnie136 <bonnie.wells@gmail.com>
2026-06-07 18:16:16 +0100

    Added select functionality in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 function updateTime() {
   let londonDateElement = document.querySelector("#london-date");
-  let londonTimeElement = document.querySelector("#london-time");
-  let londonTime = moment.tz("Europe/London");
+  if (londonDateElement) {
+    let londonTimeElement = document.querySelector("#london-time");
+    let londonTime = moment.tz("Europe/London");
 
-  londonDateElement.innerHTML = londonTime.format("Mo MMMM YYYY");
-  londonTimeElement.innerHTML = londonTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
+    londonDateElement.innerHTML = londonTime.format("Mo MMMM, YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    );
+  }
 
   let hobartDateElement = document.querySelector("#hobart-date");
-  let hobartTimeElement = document.querySelector("#hobart-time");
-  let hobartTime = moment.tz("Australia/Hobart");
+  if (hobartDateElement) {
+    let hobartTimeElement = document.querySelector("#hobart-time");
+    let hobartTime = moment.tz("Australia/Hobart");
 
-  hobartDateElement.innerHTML = hobartTime.format("Mo MMMM YYYY");
-  hobartTimeElement.innerHTML = hobartTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
+    hobartDateElement.innerHTML = hobartTime.format("Mo MMMM, YYYY");
+    hobartTimeElement.innerHTML = hobartTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let selectedTimeZone = event.target.value;
+  let cityName = selectedTimeZone.replace("_", " ").split("/").pop();
+  let selectedTime = moment().tz(selectedTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div>
+          <div class="city">
+            <span class="material-symbols-outlined"> location_on </span> ${cityName}
+          </div>
+          <div class="time">${selectedTime.format("h:mm:ss")}<small>${selectedTime.format(" A")}</small></div>
+          <div class="date">${selectedTime.format("Mo MMMM, YYYY")}</div>
+        </div>
+    `;
+}
+
+let citySelectElement = document.querySelector("#city-selector");
+citySelectElement.addEventListener("change", updateCity);
